63e94de89573b9d19029a831b3980641086d0e55
Gertjan Kruiger <gertjankruiger@gmail.com>
2026-02-25 20:04:54 +0100

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,63 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
+import { useState, useEffect } from 'react'
+import AppBar from './AppBar'
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+
+  interface State {
+    isSoundOn: boolean,
+  }
+
+  let state: State = localStorage.hasOwnProperty('state') 
+    ? JSON.parse(localStorage.getItem('state') || '{}')
+    : {
+        isSoundOn: true
+      }
+ 
+  const [isSoundOn, setIsSoundOn] = useState<boolean>(state.isSoundOn)
+ 
+  useEffect(function storeStateInLocalStorage(): void {
+    // For testing purposes, never save.
+    if(false) {  
+      localStorage.setItem(
+        'state', 
+        JSON.stringify({
+          isSoundOn: isSoundOn,
+          // There is room for more items in the state
+        })
+      )
+    }
+  }, [isSoundOn])
+
+  function restart() {
+    localStorage.removeItem('state')
+    window.location.reload()
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <div className='center'>
+            <AppBar
+              isSoundOn={isSoundOn}
+              setIsSoundOn={setIsSoundOn}
+              restart={restart}
+            />
+          </div>
+        </div>
+        <div className="content">
+          {/* 
+            TO DO: Place content.
+          */}
+        </div>
+        <div className="footer">
+          <div className='center'>
+            {/* 
+              TO DO: Place footer.
+            */}
+          </div>
+        </div>
+      </div>  
+    </>
+  )  
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,12 @@
-import { createTheme } from "@mantine/core";
+import { createTheme, rem } from "@mantine/core"
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
-});
+  fontSizes: {
+    xs: rem(10),
+    sm: rem(11),
+    md: rem(14),
+    lg: rem(16),
+    xl: rem(20),
+    xxl: rem(24)
+  },
+})
